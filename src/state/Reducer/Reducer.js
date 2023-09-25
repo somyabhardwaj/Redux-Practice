25f52382aebb9016354e10284810b54d3c2e234c
Somya Bhardwaj <somyabhardwaj004@gmail.com>
2023-09-25 11:40:24 +0530

    Reducer created

---
+++ src/state/Reducer/Reducer.js
@@ -0,0 +1,12 @@
+
+export default Reducer =(state=0 , action)=>{
+if (action.type === 'deposit'){
+    return state + action.payload;
+}
+else if (action.type === 'widthdraw'){
+   return state - action.payload;
+}
+else
+return state;
+
+}
